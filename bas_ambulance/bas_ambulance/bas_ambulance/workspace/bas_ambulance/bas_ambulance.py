ccb1dcd7a84fc9adf6c6a2ba0df5bd1ad7b5dc9a
nick200555 <nick.m.briggs@gmail.com>
2026-04-29 10:50:00 +0530

    feat: Add BAS Ambulance workspace structure

---
+++ bas_ambulance/bas_ambulance/bas_ambulance/workspace/bas_ambulance/bas_ambulance.py
@@ -0,0 +1,14 @@
+import frappe
+
+def get_data():
+	"""
+	Return the data for the workspace, like onboard charts or logic
+	if required for the BAS Ambulance system.
+	"""
+	return {}
+
+def get_onboard_data():
+	"""
+	Onboarding details.
+	"""
+	return {}
